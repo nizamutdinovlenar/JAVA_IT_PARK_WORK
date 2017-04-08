128ee52b7b8313e7af79ad448d56582b958dd168
nizamutdinovlenar <nizamutdinovlenar@gmail.com>
2017-04-08 17:29:18 +0300

    Add files via upload

---
+++ ClassWorks/exam/Program.java
@@ -0,0 +1,27 @@
+Public interface exam{
+    public static void main(String[] args) {
+        String Name();
+        doble Area();
+        double Perimeter();
+    int a;
+    int b;
+    int h;
+
+
+        Figures Figures = new Figures();
+        Figures[] myFigures = new Figures [3];
+    myFigures[0] = new Figures();
+    myFigures[1] = new Figures();
+    myFigures[2] = new Figures();
+    myFigures[0].name = "Triangle";
+    myFigures[1].name = "Kurg";
+    muFigures[2].name = "Rectangle";
+    myFigures[0].FigureArea = (a*h)/2;
+    myFigures[1].FigureArea = pi*r^2;
+    muFigures[2].FigureArea = a*b;
+    myFigures[0].FigurePerimeter = (a*h)/2;
+    myFigures[1].FigurePerimeter = pi*r^2;
+    muFigures[2].FigurePerimeter = a*b;
+
+    }
+}
